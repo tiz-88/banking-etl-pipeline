2c4b80b171b28e4c268eab616d6014521507ec57
tiz <tizzot88@hotmail.com>
2025-04-25 15:42:51 +0200

    Refactor: reorganize project into etl/ and data/ folders

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,9 +1,9 @@
-from extract_transactions import extract_transactions
-from transform_transactions import transform_transactions
-from load_transactions import load_to_db
+from etl.extract_transactions import extract_transactions
+from etl.transform_transactions import transform_transactions
+from etl.load_transactions import load_to_db
 
 def run_etl():
-    df_raw = extract_transactions('fake_transactions.csv')
+    df_raw = extract_transactions('data/fake_transactions.csv')
     df_clean = transform_transactions(df_raw)
     load_to_db(df_clean)
 
